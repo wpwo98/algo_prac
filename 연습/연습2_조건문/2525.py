ffee2e538932c3da26907062b240d3474bbd2c44
wpwo98 <62274357+wpwo98@users.noreply.github.com>
2022-11-04 22:31:28 +0900

    Delete 2525.py

--- 연습/연습2_조건문/2525.py
+++
@@ -1,9 +0,0 @@
-A, B = map(int, input().split())
-C = int(input())
-
-if (B+C)<60 :
-    print(A, B+C)
-    
-elif A+int((B+C)/60) >= 24 :
-    print(A+int((B+C)/60) - 24, B+C-int((B+C)/60)*60)
-else: print(A+int((B+C)/60), B+C-int((B+C)/60)*60)
